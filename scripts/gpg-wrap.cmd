fa737da4d6afb70e096ae591064a2a4149c45242
Alexander Railean <alexander.railean@siemens.com>
2023-03-22 13:23:47 +0100

    Suppress a false positive detection of CVE-2022-45688 + cleanup

--- scripts/gpg-wrap.cmd
+++ scripts/gpg-wrap.cmd
@@ -1,7 +1,7 @@
 REM This is a thin wrapper around gpg-wrap.ps1, it is necessary because it seems that on Windows Maven cannot execute
 REM a powershell script directly, as a standalone executable. What this file does is simply pass its arguments.
-REM The Runner executes it from within the checked out project's top-level directory, so the relative path used below
-REM is acceptable.
+REM The Github Runner executes it from within the checked out project's top-level directory, so the relative path used
+REM below is acceptable.
 
 REM REQUIREMENT
 REM - This file must be copied to a location that is accessible in the system's PATH environment variable, such that
