8ae45b149423c49f3b5ac6870e0daec990300e4b
Zoltan Maradics <zoltan.maradics@siemens.com>
2026-04-14 10:09:33 +0200

    Add header file and spotless config

--- src/main/java/com/siemens/pki/cmpracomponent/msgprocessing/RaDownstream.java
+++ src/main/java/com/siemens/pki/cmpracomponent/msgprocessing/RaDownstream.java
@@ -1,19 +1,19 @@
 /*
- *  Copyright (c) 2022 Siemens AG
+ * Copyright (c) 2026 Siemens AG
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may
- *  not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *  SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: Apache-2.0
  */
 package com.siemens.pki.cmpracomponent.msgprocessing;
 
@@ -645,13 +645,16 @@ class RaDownstream {
                     "InventoryInterface.checkRevocationRequest(byte[], String, String, String, byte[])",
                     () -> inventory.checkRevocationRequest(
                             persistencyContext.getTransactionId(),
-                            ifNotNull(incomingRequest.getHeader().getSender(), sender -> X500Name.getInstance(
-                                            sender.getName())
-                                    .toString()),
-                            ifNotNull(revTemplate, template -> template.getSerialNumber()
-                                    .toString()),
-                            ifNotNull(revTemplate, template -> template.getIssuer()
-                                    .toString()),
+                            ifNotNull(
+                                    incomingRequest.getHeader().getSender(),
+                                    sender -> X500Name.getInstance(sender.getName())
+                                            .toString()),
+                            ifNotNull(
+                                    revTemplate,
+                                    template -> template.getSerialNumber().toString()),
+                            ifNotNull(
+                                    revTemplate,
+                                    template -> template.getIssuer().toString()),
                             encodedIncomingRequest))) {
                 throw new CmpValidationException(
                         INTERFACE_NAME, PKIFailureInfo.badRequest, "request refused by external inventory");
